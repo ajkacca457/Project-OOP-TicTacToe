5dbcb4c3ac3fdda126477ad240826fa1eddba3b5
rick <rickoburu@gmaol.com>
2020-09-23 15:15:38 +0300

    Added random contion to determine whose to begin

--- bin/main.rb
+++ bin/main.rb
@@ -28,3 +28,18 @@ player_two = gets.chomp
 # Creation of a new player object with the given name if the given name is not empty or nil
 
 3.downto(1) { |counter| puts "wait #{counter} second" }
+
+puts " today our players are : #{player_one} and #{player_two}"
+
+puts 'Lets have a toss to select who will go first..'
+
+# A random condition will will determine who will go first. X will be assign to player1 and O will be assign to player2.
+result=rand(1..2)
+puts result==1 ? "#{player_one} will go first": "#{player_two} will go first"
+gamer_one= result==1?player_one:player_two
+gamer_two= result==1?player_two:player_one
+
+puts "#{gamer_one} will start first with character X and #{gamer_two} will go second with character O"
+
+
+3.downto(1){|counter| puts "wait #{counter} second preparing the game board"}
