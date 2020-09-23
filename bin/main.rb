bb8ae7e0def1afe4a39c7e5a67d5abb5ad37dfec
Avijit Karmaker <ajkacca@gmail.com>
2020-09-23 15:10:46 +0300

    mockup for player name created and player names stored in separate variables

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,30 @@
 #!/usr/bin/env ruby
 puts '"Hello world!"'
 
-#Setting up the game initially
-    # Create a game board class
-    # Create a couple players
-    # Create the game class
+# Setting up the game initially
+# Create a game board class
+# Create a couple players
+# Create the game class
 
-    puts 'Welcome!! Are you ready to play tic-tac-toe?'
-    player_answer=gets.chomp.upcase
-    
-    if player_answer=="YES" || player_answer=="Y"
-      puts "welcome to the game"
-    else
-      puts "Hope you will return soon"
-    end
+puts 'Welcome!! Are you ready to play tic-tac-toe?'
+player_answer = gets.chomp.upcase
+
+if player_answer == 'yes'
+  puts 'welcome to the game'
+else
+  puts 'Hope you will return soon'
+end
+
+# creating players object-1
+puts 'enter name of first_player'
+player_one = gets.chomp
+# Validation of the input and convert the name to uppercase letter
+# Creation of a new player object with the given name if the given name is not empty or nil
+
+# creating players object-2
+puts 'enter name of second_player'
+player_two = gets.chomp
+# Validation of the input and convert the name to uppercase letter
+# Creation of a new player object with the given name if the given name is not empty or nil
+
+3.downto(1) { |counter| puts "wait #{counter} second" }
