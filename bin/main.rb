77d0acca62ac7fae9f12dc5e001071a177250b84
rick <rickoburu@gmaol.com>
2020-09-23 14:54:15 +0300

    added iniatial welcome to ttt text

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
 puts '"Hello world!"'
+
+#Setting up the game initially
+    # Create a game board class
+    # Create a couple players
+    # Create the game class
+
+    puts 'Welcome!! Are you ready to play tic-tac-toe?'
+    player_answer=gets.chomp.upcase
+    
+    if player_answer=="YES" || player_answer=="Y"
+      puts "welcome to the game"
+    else
+      puts "Hope you will return soon"
+    end
