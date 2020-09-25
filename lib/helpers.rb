3cf4cf7986683901732982bafc32771565e50e26
RICKCOYL <rickoburu@gmail.com>
2020-09-25 17:11:02 +0300

    added game validation for user-input

---
+++ lib/helpers.rb
@@ -0,0 +1,22 @@
+class Helpers
+
+    def valid_move?(board, index)
+      index.between?(0, 8) && !position_taken?(board, index)
+    end
+    
+    def position_taken?(board, index)
+     if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+        return false
+     else
+        return true
+     end
+    end
+    
+    def input_to_index(input)
+      input.to_i - 1
+    end
+    
+    
+    
+end
+    
