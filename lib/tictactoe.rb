73e59b95b84cbf18990b4a931ef3222b02b26126
RICKCOYL <rickoburu@gmail.com>
2020-09-25 17:18:40 +0300

    added the turn method

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -24,7 +24,36 @@ class Tictactoe
     sleep 1
   end
 
+  def move_board(index)
+    @board.board[index] = @player.sign
+  end
 
+  def turn
+      @player = @player == @name[0] ? @name[1] : @name[0]
+      puts "#{@player.name}, choose a spot between 1-9"
+      player_move = gets.strip.to_i
+      if !player_move.positive?
+        puts 'Please enter a valid move'
+        player_move = gets.strip.to_i
+      end
+      if !@helpers.valid_move?(@board.board, @helpers.input_to_index(player_move))
+        puts "#{@player.name}, choose a spot between 1-9 which is not taken"
+        player_move = gets.strip.to_i
+      end
+      move_board(@helpers.input_to_index(player_move))
+      puts `clear`
+      @board.display_board
+
+
+
+    if @game.won?(@board.board)
+        puts "Congratulations #{@player.name} you won!!!"
+        @game_over = true
+      elsif @game.draw?(turn_count)
+        puts "It is a Draw!!!"
+        @game_over = true
+      end
+  end
 
 
 
