917d823a66fec9d443d62802851b388bbc9ac163
Avijit <ajkacca@gmail.com>
2020-09-27 01:57:16 +0300

    user interface updated based on the review

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,8 @@
 #!/usr/bin/env ruby
+require_relative '../lib/board'
+require_relative '../lib/helpers'
 
 puts '"Hello world!"'
-
-# Setting up the game initially
-# Create a game board class
-# Create a couple players
-# Create the game class
 
 puts 'Welcome!! Are you ready to play tic-tac-toe?'
 player_answer = gets.chomp.upcase
@@ -19,14 +16,10 @@ end
 # creating players object-1
 puts 'enter name of first_player'
 player_one = gets.chomp
-# Validation of the input and convert the name to uppercase letter
-# Creation of a new player object with the given name if the given name is not empty or nil
 
 # creating players object-2
 puts 'enter name of second_player'
 player_two = gets.chomp
-# Validation of the input and convert the name to uppercase letter
-# Creation of a new player object with the given name if the given name is not empty or nil
 
 3.downto(1) { |counter| puts "wait #{counter} second" }
 
@@ -34,23 +27,18 @@ puts " today our players are : #{player_one} and #{player_two}"
 
 puts 'Lets have a toss to select who will go first..'
 
-# A random condition will will determine who will go first. X will be assign to player1 and O will be assign to player2.
+# A random condition will will determine who will go first.
+# X will be assign to player1 and O will be assign to player2.
+
 result = rand(1..2)
 puts result == 1 ? "#{player_one} will go first" : "#{player_two} will go first"
 gamer_one = result == 1 ? player_one : player_two
 gamer_two = result == 1 ? player_two : player_one
 
-puts "#{gamer_one} will start first with character X and #{gamer_two} will go second with character O"
+puts "#{gamer_one} will start first with character X"
+puts "#{gamer_two} will go second with character O"
 
 3.downto(1) { |counter| puts "wait #{counter} second preparing the game board" }
-
-# Will create the board from board class which will take an array as argument
-# board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# or board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-# In order to game to be work the game object will require a list of winning combinations.
-
-# Game class will store the following winning combinations
 
 # winning_combinations = [
 #   [1, 2, 3],
@@ -64,66 +52,29 @@ puts "#{gamer_one} will start first with character X and #{gamer_two} will go se
 # ]
 
 # after setting up the board then the game will set up:
-# initialize a variable
+
 winner = nil
-# winner variable will change to the player if a player gets winning combinations
-# initialize a variable
-counter = 0
 
 # board will be render for the players
+board = Board.new
+board.display_board
 
-puts '1 | 2 | 3'
-puts '----------'
-puts '4 | 5 | 6'
-puts '----------'
-puts '7 | 8 | 9'
+helpers = Helpers.new
 
 # after the setup game will start a loop to get player inputs until:
-# there is a winning combination
-# All the cell of the board is taken
-
-while counter < 9
-  p 'For your move, Enter a number between 1 and 9.'
-  counter += 1
-  if counter.even?
-    # gamer_one will enter his/her move
-
-    # Getting the input of present_player_one
-
-    # we will validate the input by checking whether it is not nil or false or not anything except 1 to 9
-    # we will check the board whether the move match the board index and then put the move on the board.
-    gamer_one_move = gets.chomp # (gamer_one or gamer_two depending upon the toss)
-    p "#{gamer_one} made the move #{gamer_one_move}"
-
-    # we will update the board based on the move made by present_player_one
-    # board.update_move(gamer_one_move.to_i - 1, gamer_one_sym#(X))
-
+9.times do
+  puts 'enter any number from 1 to 9'
+  player1_move = gets.chomp
+  if helpers.position_taken?(board.board, helpers.input_to_index(player1_move))
+    puts 'position not available. Enter another number'
   else
-    # present_player_two will make the move
-    # we will check the board whether the move match the board index and then put the move on the board.
-    gamer_two_move = gets.chomp # (gamer_one or gamer_two depending upon the toss)
-    p "#{gamer_two} made the move #{gamer_two_move}"
-    # we will update the board based on the move made by present_player_two
-    # board.update_move(gamer_two_move.to_i - 1, gamer_two_sym#(O))
+    board.board[helpers.input_to_index(player1_move)] = player1_move
+    board.display_board
   end
 end
 
-# we will break out of the loop:
-# if present_player_one/present_player_two gets a winning combination.
-# or board ran out of empty slots.
-
 if winner.nil?
-  puts 'The game was a draw!'
-  # option for restarting the game
-  # puts 'Do you want to play again?'
-  # response = gets.chomp
-  # If the response is Yes or Y we start the game again (Game.start)
-
+  puts 'the game is a draw'
 else
-  puts "the winner is #{winner}!"
-  puts "Congratulations!! #{gamer_one}"
-  # option for restarting the game
-  # puts 'Do you want to play again?'
-  # response = gets.chomp
-  # If the response is Yes or Y we start the game again (Game.start)
+  puts 'congratualtions player one/two win'
 end
