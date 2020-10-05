2dd5bb7464fc8c8abac15e07e428cbe2c5867c36
RICKCOYL <rickoburu@gmail.com>
2020-10-05 11:28:43 +0300

    add test for position_taken? method

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -6,4 +6,10 @@ describe Helpers do
             expect(subject.valid_move?("X", 10)).to be_falsey
         end
     end
+
+    describe "#position_taken?" do
+        it "Checks if position is taken" do
+            expect(subject.position_taken?(9,9)).to be_truthy
+        end
+    end
 end
